e3492ffe27023eab73330519bd014627402a1b89
Shouvik Ghosh <shouvikg.cal@gmail.com>
2023-02-28 18:42:49 +0530

    Added flask test

--- Assignment3/test_.py
+++ Assignment3/test_.py
@@ -2,6 +2,11 @@ import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from score import score
+import urllib.request
+import urllib.parse
+from subprocess import Popen
+import time
+import json
 
 # Model used for testing.
 train_x = []
@@ -29,3 +34,18 @@ def test_score():
 
     (prediction, _) = score("BUY CARS AT A DISCOUNT. Get a free test drive today. 100 lucky customers will win prizes, like free water bottles, free bags, free shoes, free books, free laptops and various other free stuff. You can win free items too.", model_logistic_regression, 0.5)
     assert prediction
+
+# https://www.howtogeek.com/devops/linux-signals-hacks-definition-and-more/
+# https://docs.python.org/3/library/subprocess.html#popen-constructor
+# https://docs.python.org/3/library/json.html?highlight=json#module-json
+# https://docs.python.org/3/library/urllib.html?highlight=urllib#module-urllib
+def test_flask():
+    process = Popen(["py", "-m", "flask", "--app", "app", "run"])
+    time.sleep(10)
+    data = urllib.parse.urlencode({'text': 'Hi'})
+    data = data.encode('ascii')
+    with urllib.request.urlopen("http://127.0.0.1:5000/score", data) as f:
+        response = f.read().decode('utf-8')
+        response_json = json.loads(response)
+        assert 'prediction' in response_json and 'propensity' in response_json
+    process.terminate()
